2d3093bd67a6a13d8afb811d7650330c0e7a1b50
Cristian <cescobarl2@ucentral.edu.co>
2024-05-29 20:55:10 -0500

    CORS solucionado

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from routes.nomina import router
 from routes.producto import producto
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1",
+    "null",  # Agrega 'null' para permitir el origen 'null' en algunos navegadores en entorno local
+    # Agrega otros orígenes que necesites
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permite estos orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, PUT, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 app.include_router(router)
 #app.include_router(producto)
